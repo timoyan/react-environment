f177a5a98f15a59fcc8fac32bf5ebd7911dc5b9a
timotcyan <>
2018-04-12 02:12:31 +0800

    add reactstrap and wizard into modal

--- src/app.jsx
+++ src/app.jsx
@@ -5,8 +5,25 @@ import Headers from "./components/common/Header";
 import ProductList from "./components/product/ProductList";
 import NewsList from "./components/news/NewsList";
 import Wizard from "./components/common/Wizard";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import "./styles.scss";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
   render() {
     return (
       <div className="AppContainer">
@@ -15,7 +32,31 @@ class App extends React.Component {
           <Route path="/product" component={ProductList} />
           <Route path="/news" component={NewsList} />
         </Switch>
-        <Wizard />
+
+        <div>
+          <Button color="danger" onClick={this.toggle}>
+            Test Modal
+          </Button>
+          <Modal
+            isOpen={this.state.modal}
+            toggle={this.toggle}
+            centered={true}
+            className="mw-70p"
+          >
+            <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+            <ModalBody>
+              <Wizard />
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={this.toggle}>
+                Do Something
+              </Button>{" "}
+              <Button color="secondary" onClick={this.toggle}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </Modal>
+        </div>
       </div>
     );
   }
